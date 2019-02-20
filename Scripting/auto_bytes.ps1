753e6531115d3213bb9a21f41f0f4a1c471fc650
Robert <robertpitre7@gmail.com>
2019-02-20 13:07:54 -0600

    Added commandline parameters and variables to make execution more flexible

--- Scripting/auto_bytes.ps1
+++ Scripting/auto_bytes.ps1
@@ -1,9 +1,20 @@
-﻿# Move the installer file from the "server" to the "machine" that needs to be scanned
+﻿### Variables ###
+$transloc = "C:\Users\Robert\Documents\Winter2019\CSC405\Capstone\Script\testing\installer"
+$destloc = "C:\Testing"
+$malInstLoc = "C:\Program Files (x86)\Malwarebytes' Anti-Malware" 
+
+param(
+[Parameter(Mandatory=$true)][string]$compname,
+[Parameter(Mandatory=$true)][string]$liscID,
+[Parameter(Mandatory=$true)][string]$liscKey
+)
+
+# Move the installer file from the "server" to the "machine" that needs to be scanned
 # creates a new folder in the C: for usage
-Copy-Item "C:\Users\Robert\Documents\Winter2019\CSC405\Capstone\Script\testing\installer" -Destination "C:\Testing" -Recurse
+Copy-Item $transloc -Destination $destloc -Recurse
 
 # go to the directory where the installer is
-cd C:\Testing
+cd $destloc
 
 # run the installer with options:
 # 1. being able to cancel
@@ -14,12 +25,12 @@ Write-Host "Installing MalwareBytes"
 .\mbam-setup-1.80.2.1012.exe /nocancel /norestart /verysilent /suppressmsgboxes /log="C:\Testing\install_log.txt"
 
 Start-Sleep -Seconds 10
-cd "C:\Program Files (x86)\Malwarebytes' Anti-Malware"
+cd $malInstLoc
 
 Write-Host "Waiting"
 Start-Sleep -Seconds 2
 Write-Host "Registering"
-.\mbamapi.exe /register #ADD ID AND KEY SYNTAX: <ID> <KEY>
+.\mbamapi.exe /register $liscID $liscKEY #ADD ID AND KEY SYNTAX: <ID> <KEY>
 
 #Start-Sleep -Seconds 3
 Write-Host "Hide Registration Details"
@@ -31,7 +42,7 @@ Write-Host "Updating"
 
 #Start-Sleep -Seconds 3
 Write-Host "Changing Log File Location"
-.\mbamapi.exe /logtofolder C:\Testing
+.\mbamapi.exe /logtofolder $destloc
 
 #Run scan
 Write-Host "Starting Scan"
@@ -43,7 +54,7 @@ Write-Host "Uninstalling"
 Write-Host "Uninstall Complete"
 
 #Move to cleanup directory
-cd C:\Testing
+cd $destloc
 
 #This program cleans up the programs and performs reboot
 .\mbstcmd.exe /y /cleanup
